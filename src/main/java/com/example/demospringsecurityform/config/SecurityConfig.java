623433223c23f028931211a7aa86cd943d43f4b6
055055 <snb4301@gmail.com>
2019-10-26 21:43:58 +0900

    스프링 시큐리티 커스터마이징 : JPA 연동

--- src/main/java/com/example/demospringsecurityform/config/SecurityConfig.java
+++ src/main/java/com/example/demospringsecurityform/config/SecurityConfig.java
@@ -12,22 +12,12 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.authorizeRequests()
-                    .mvcMatchers("/","/info").permitAll()
+                    .mvcMatchers("/","/info","/account/**").permitAll()
                     .mvcMatchers("/admin").hasRole("ADMIN")
                     .anyRequest().authenticated()
                     .and()
                 .formLogin()
                     .and()
                 .httpBasic();
-    }
-
-
-    @Override
-    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-        auth.inMemoryAuthentication()
-                .withUser("055055").password("{noop}123").roles("USER").and()
-                .withUser("admin").password("{noop}!@#").roles("ADMIN");
-                //{noop} 암호화 인코딩을 사용하지 않겠다고 명시 한것. {}안에는 암호화 인코딩을 적을 수 있다. 그러면 비밀번호가 해당 암호화 방식으로 인코딩됨
-
     }
 }
