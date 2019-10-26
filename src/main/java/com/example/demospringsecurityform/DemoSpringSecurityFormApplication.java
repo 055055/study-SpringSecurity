9491c7d0f2de1dabc359bb502d5e76a4fb3ef477
055055 <snb4301@gmail.com>
2019-10-26 21:52:46 +0900

    스프링 시큐리티 커스터마이징: PasswordEncoder

--- src/main/java/com/example/demospringsecurityform/DemoSpringSecurityFormApplication.java
+++ src/main/java/com/example/demospringsecurityform/DemoSpringSecurityFormApplication.java
@@ -2,9 +2,18 @@ package com.example.demospringsecurityform;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.crypto.factory.PasswordEncoderFactories;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @SpringBootApplication
 public class DemoSpringSecurityFormApplication {
+
+	@Bean
+	public PasswordEncoder passwordEncoder(){
+		return PasswordEncoderFactories.createDelegatingPasswordEncoder(); //다양한 암호화 알고리즘을 제공한다. 기본적으로는 bcrypt로 암호화한다.
+	}
+
 
 	public static void main(String[] args) {
 		SpringApplication.run(DemoSpringSecurityFormApplication.class, args);
