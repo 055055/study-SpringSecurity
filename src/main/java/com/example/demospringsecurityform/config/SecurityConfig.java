e257361531cb781f45c4dcab9935ff63389b9669
055055 <snb4301@gmail.com>
2019-10-26 21:13:23 +0900

    스프링 시큐리티 커스터마이징 : 인메모리 유저 추가

--- src/main/java/com/example/demospringsecurityform/config/SecurityConfig.java
+++ src/main/java/com/example/demospringsecurityform/config/SecurityConfig.java
@@ -1,6 +1,7 @@
 package com.example.demospringsecurityform.config;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
@@ -18,5 +19,15 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .formLogin()
                     .and()
                 .httpBasic();
+    }
+
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.inMemoryAuthentication()
+                .withUser("055055").password("{noop}123").roles("USER").and()
+                .withUser("admin").password("{noop}!@#").roles("ADMIN");
+                //{noop} 암호화 인코딩을 사용하지 않겠다고 명시 한것. {}안에는 암호화 인코딩을 적을 수 있다. 그러면 비밀번호가 해당 암호화 방식으로 인코딩됨
+
     }
 }
