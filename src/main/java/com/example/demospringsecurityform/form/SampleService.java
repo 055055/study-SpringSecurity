467ebbf8207743adc611826d72cd105de57df546
055055 <snb4301@gmail.com>
2019-11-01 21:51:31 +0900

    스프링 시큐리티 : 아키텍처 ThreadLocal

--- src/main/java/com/example/demospringsecurityform/form/SampleService.java
+++ src/main/java/com/example/demospringsecurityform/form/SampleService.java
@@ -2,13 +2,18 @@ package com.example.demospringsecurityform.form;
 
 import com.example.demospringsecurityform.account.Account;
 import com.example.demospringsecurityform.account.AccountContext;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Service;
+import sun.plugin.liveconnect.SecurityContextHelper;
 
 @Service
 public class SampleService {
     public void dashboard() {
-        Account account = AccountContext.getAccount();
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        UserDetails userDetails = (UserDetails)authentication.getPrincipal(); //principal은 object type이기 떄문에 UserDetails 또는 USer로 타입변환.
         System.out.println("=========");
-        System.out.println(account.getUsername());
+        System.out.println(userDetails.getUsername());
     }
 }
