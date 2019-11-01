103929b88c5cece05e1b1fe524f3001cfb170c3b
055055 <snb4301@gmail.com>
2019-11-01 22:00:19 +0900

    스프링 시큐리티 : 아키텍처 AccessDecisionManager 	2부  2차 커밋 SecurityExpressionHandler 커스터마이징

--- src/main/java/com/example/demospringsecurityform/config/SecurityConfig.java
+++ src/main/java/com/example/demospringsecurityform/config/SecurityConfig.java
@@ -3,6 +3,7 @@ package com.example.demospringsecurityform.config;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.access.AccessDecisionManager;
 import org.springframework.security.access.AccessDecisionVoter;
+import org.springframework.security.access.expression.SecurityExpressionHandler;
 import org.springframework.security.access.hierarchicalroles.RoleHierarchyImpl;
 import org.springframework.security.access.vote.AffirmativeBased;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
@@ -18,18 +19,18 @@ import java.util.List;
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
-    public AccessDecisionManager accessDecisionManager(){
+
+    public SecurityExpressionHandler expressionHandler(){
         RoleHierarchyImpl roleHierarchy = new RoleHierarchyImpl();
         roleHierarchy.setHierarchy("ROLE_ADMIN > ROLE_USER");
         DefaultWebSecurityExpressionHandler handler = new DefaultWebSecurityExpressionHandler();
         handler.setRoleHierarchy(roleHierarchy);
 
-        WebExpressionVoter webExpressionVoter = new WebExpressionVoter();
-        webExpressionVoter.setExpressionHandler(handler);
+        return handler;
 
-        List<AccessDecisionVoter<? extends  Object>> voters = Arrays.asList(webExpressionVoter);
-        return new AffirmativeBased(voters);
     }
+
+
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
@@ -38,10 +39,9 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                     .mvcMatchers("/admin").hasRole("ADMIN")
                     .mvcMatchers("/user").hasRole("USER")
                 .anyRequest().authenticated()
-                .accessDecisionManager(accessDecisionManager())
-                    .and()
-                .formLogin()
-                    .and()
-                .httpBasic();
+                .expressionHandler(expressionHandler());
+
+                http.formLogin();
+                http.httpBasic();
     }
 }
