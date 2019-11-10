c135f59c1eb02f9999a455e943f4e88b8f28b200
055055 <snb4301@gmail.com>
2019-11-10 18:14:09 +0900

    스프링 시큐리티 웹 애플리케이션 시큐리티 : 인증/인가 예외 처리 필터 ExceptionTranslationFilter

--- src/main/java/com/example/demospringsecurityform/config/SecurityConfig.java
+++ src/main/java/com/example/demospringsecurityform/config/SecurityConfig.java
@@ -4,6 +4,7 @@ import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.access.AccessDecisionManager;
 import org.springframework.security.access.AccessDecisionVoter;
+import org.springframework.security.access.AccessDeniedException;
 import org.springframework.security.access.expression.SecurityExpressionHandler;
 import org.springframework.security.access.hierarchicalroles.RoleHierarchyImpl;
 import org.springframework.security.access.vote.AffirmativeBased;
@@ -13,9 +14,15 @@ import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.web.access.AccessDeniedHandler;
 import org.springframework.security.web.access.expression.DefaultWebSecurityExpressionHandler;
 import org.springframework.security.web.access.expression.WebExpressionVoter;
 
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
 import java.util.Arrays;
 import java.util.List;
 
@@ -62,6 +69,19 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
                 http.sessionManagement()
                     .maximumSessions(1);  //동시성 제어. 한 계정 동시 접속 1명으로 제한
+
+
+                http.exceptionHandling()
+                       // .accessDeniedPage("/access-denied");  // 페이지에서 인증이 거부 되었을 때 보여주는 페이지. default는 화이트 에러 페이지.. 이렇게하면 로그를 확인하지 못함.
+                         .accessDeniedHandler(new AccessDeniedHandler() { //accessDeniedHandler를 사용하면 아래와 같이 에러로그도 남기고 페이지로 넘길 수 도 있음
+                             @Override
+                             public void handle(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse, AccessDeniedException e) throws IOException, ServletException {
+                                UserDetails principal =  (UserDetails) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+                                String username = principal.getUsername();
+                                 System.out.println(username + "is denied to access"+httpServletRequest.getRequestURI());
+                                 httpServletResponse.sendRedirect("/access-denied");
+                             }
+                         });
 
 
         SecurityContextHolder.setStrategyName(SecurityContextHolder.MODE_INHERITABLETHREADLOCAL); //현재 쓰레드안의 하위 쓰레드에까지 씨큐리티컨텍스트를 공유하는 전략 for @Async
